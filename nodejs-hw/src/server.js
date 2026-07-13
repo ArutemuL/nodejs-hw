2d5a2609477b9f9453f13a69f6353f318d9fd530
ArutemuL <lvovartem002@gmail.com>
2026-07-13 20:22:38 +0300

    .

--- nodejs-hw/src/server.js
+++ nodejs-hw/src/server.js
@@ -11,16 +11,19 @@ app.use(cors());
 app.use(
   pino({
     level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
+    transport:
+      process.env.NODE_ENV !== 'production'
+        ? {
+            target: 'pino-pretty',
+            options: {
+              colorize: true,
+              translateTime: 'HH:MM:ss',
+              ignore: 'pid,hostname',
+              messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+              hideObject: true,
+            },
+          }
+        : undefined,
   }),
 );
 
